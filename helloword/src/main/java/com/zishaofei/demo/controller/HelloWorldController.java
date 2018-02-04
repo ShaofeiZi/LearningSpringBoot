693cfb3ab58e37e950346002215fb2e36e60d1e9
訾绍飞 <zishaofei@condordya.com>
2018-02-04 22:47:05 +0800

    test

--- helloword/src/main/java/com/zishaofei/demo/controller/HelloWorldController.java
+++ helloword/src/main/java/com/zishaofei/demo/controller/HelloWorldController.java
@@ -3,8 +3,11 @@ package com.zishaofei.demo.controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+//RestController的意思就是controller里面的方法都以json格式输出，不用再写什么jackjson配置的了！
 @RestController
+
 public class HelloWorldController {
+    //    路由匹配
     @RequestMapping("/hello")
     public String index() {
         return "Hello World";
